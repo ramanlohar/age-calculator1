e9c7796b8bf61bc1925fe413bf3d40cb37dc84c8
ramanlohar <ramanlohar11@gmail.com>
2023-11-02 15:58:18 +0530

    https://www.youtube.com/

--- JS/hen.js
+++ JS/hen.js
@@ -3,9 +3,9 @@ const headinput_name = document.querySelector(".headinput_name");
 headinput_name.value = localStorage.getItem("Header_input_name");
 
 headinput_name.addEventListener("input", () => {
-    if (headinput_name.value === "12345") {
+    if (headinput_name.value === "yt1234") {
         setTimeout(() => {
-            window.location.href = "https://github.com/ramanlohar/age-calculator1";
+            window.location.href = "https://www.youtube.com/";
         }, 100);
     }
     else{        
