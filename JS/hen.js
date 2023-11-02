874e7bbd31f230beb560eb30e64aab8b297be966
ramanlohar <ramanlohar11@gmail.com>
2023-11-02 16:24:55 +0530

    #

--- JS/hen.js
+++ JS/hen.js
@@ -3,7 +3,7 @@ const headinput_name = document.querySelector(".headinput_name");
 headinput_name.value = localStorage.getItem("Header_input_name");
 
 headinput_name.addEventListener("input", () => {
-    if (headinput_name.value === "yt1234") {
+    if (headinput_name.value === "#yt1234") {
         setTimeout(() => {
             window.location.href = "https://www.youtube.com/";
         }, 100);
