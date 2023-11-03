94390049c8962ce6582028c7b2f4cef10ffcc74e
ramanlohar <ramanlohar11@gmail.com>
2023-11-03 10:29:07 +0530

    Update hen.js

--- JS/hen.js
+++ JS/hen.js
@@ -1,14 +1,49 @@
 const headinput_name = document.querySelector(".headinput_name");
 
+// Set the initial value from local storage
 headinput_name.value = localStorage.getItem("Header_input_name");
 
+window.onload = function () {
+    // location.reload();
+    headinput_name.value = localStorage.getItem("Header_input_name");
+};
+
 headinput_name.addEventListener("input", () => {
-    if (headinput_name.value === "#yt1234") {
-        setTimeout(() => {
-            window.location.href = "https://www.youtube.com/";
-        }, 100);
-    }
-    else{        
-        localStorage.setItem("Header_input_name", headinput_name.value);
+    // Convert the input value to lowercase for case-insensitive comparison
+    const inputValue = headinput_name.value.toLowerCase();
+    if (inputValue.charAt(0) === "#") {
+        if (inputValue === "#google@1234#") {
+            window.location.href = "https://www.google.com/";
+        } else if (inputValue === "#yt@1234#") {
+            setTimeout(() => {
+                window.location.href = "https://www.youtube.com/";
+            }, 100);
+        }
     }
 });
+
+headinput_name.addEventListener("change", () => {
+    // Convert the input value to lowercase for case-insensitive comparison
+    const inputValue = headinput_name.value.toLowerCase();
+    
+    if (inputValue.charAt(0) === "#") {
+        if (inputValue === "#google@1234#") {
+            window.location.href = "https://www.google.com/";
+        } else if (inputValue === "#yt@1234#") {
+            setTimeout(() => {
+                window.location.href = "https://www.youtube.com/";
+            }, 100);
+        }
+    } else {
+        // Save the input value to local storage
+        localStorage.setItem("Header_input_name", inputValue);
+
+        // if (inputValue === "") {
+        //     // If the input is empty, remove the value from local storage
+        //     localStorage.removeItem("Header_input_name");
+        // }
+    }
+});
+
+// Get the initial value from local storage
+const head_name = localStorage.getItem("Header_input_name");
